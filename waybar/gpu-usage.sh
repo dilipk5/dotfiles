b9fa53da78d3f0ffb75f51b8bad7e380ff2cb96e
dilipk5 <dilipsc02@gmail.com>
2025-12-02 05:15:22 +0000

    cronjobs commit

--- waybar/gpu-usage.sh
+++ waybar/gpu-usage.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-gpu_usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
-echo "{\"text\": \"GPU ${gpu_usage}%\", \"tooltip\": \"GPU Usage\"}"
-
+/home/panda/.config/waybar/gpu-usage.sh
